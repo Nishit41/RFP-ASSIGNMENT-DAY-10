c3793c206e9ee3b7ae1c42395e52355ca769e5d1
Nishit <nishitranjan41@gmail.com>
2022-07-16 23:17:26 +0530

    Welcome to AddressBookSystem

---
+++ src/bridgelabz/com/basics/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package bridgelabz.com.basics;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("welcome to address book system");
+    }
+}
